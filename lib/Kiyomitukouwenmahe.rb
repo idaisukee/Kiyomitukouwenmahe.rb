ce1a8991eced11c943a9ad97d27d8aeb532da9fe
Daisuke Inoue <hibariyashiki@gmail.com>
2018-09-24 20:13:55 +0900

    finish

--- lib/Kiyomitukouwenmahe.rb
+++ lib/Kiyomitukouwenmahe.rb
@@ -1,49 +1,21 @@
 # coding: utf-8
-tower = Range.new('A', 'E').to_a
+towers = Range.new('A', 'E').to_a
 
-floor = Range.new('1', '5').to_a
+floors = Range.new('1', '5').to_a
 
-side = ["内", "外"]
+sides = ["内", "外"]
 
-floorroom = ["B1", "D1", "E3", "E4"]
+floor_rooms = ["B1", "D1", "E3", "E4"]
 
-unused = ["A1内"]
+unused_blocks = ["A1内"]
 
 
-block = tower.product(floor).product(side).map do |i|
+blocks = towers.product(floors).product(sides).map do |i|
 	i.flatten.join
 end
 
-for t in tower
+rooms = blocks - (floor_rooms.product(sides)).map do |i|
+	i.flatten.join
+end - unused_blocks + floor_rooms
 
-	for f in floor
-
-		for s in side
-
-			block << t + f.to_s + s
-
-		end
-
-	end
-
-end
-
-floorroomdummy = []
-
-for f in floorroom
-
-	for s in side
-
-		floorroomdummy << f + s
-
-	end
-
-end
-
-real = (block - floorroomdummy + floorroom - unused).sort
-
-for r in real
-
-	print r , "\n"
-
-end
+p rooms.sort
